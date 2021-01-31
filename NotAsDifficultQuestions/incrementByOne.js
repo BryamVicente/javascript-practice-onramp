50dd8c78f3fd32ca1ce9c3cc36ab94186bb70678
github username <github email address>
2021-01-30 22:16:45 -0500

    done with easy questions

---
+++ NotAsDifficultQuestions/incrementByOne.js
@@ -0,0 +1,11 @@
+/*
+Create a function that takes a number as an argument, 
+increments the number by +1 and returns the result.
+*/
+
+function addition(num) {
+    let incrementedNumber = num + 1
+    return incrementedNumber
+}
+
+console.log(addition(10))
