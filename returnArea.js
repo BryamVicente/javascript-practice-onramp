89d0c944dd8cfeb1fa333309abae00f16555ffe2
github username <github email address>
2021-01-30 21:26:01 -0500

    1st question done

---
+++ returnArea.js
@@ -0,0 +1,13 @@
+
+/*
+Write a function that takes the base and height 
+of a triangle and return its area.
+*/
+
+
+function triArea(base, height) {
+	let area = base * height / 2 
+	return area 
+}
+
+console.log(triArea(30, 40))
