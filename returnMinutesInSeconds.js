0d644cfdf9bc7994c2d50e33f805ab5f583f2096
github username <github email address>
2021-01-30 21:44:36 -0500

    finished third question

---
+++ returnMinutesInSeconds.js
@@ -0,0 +1,11 @@
+/*
+Write a function that takes an integer minutes 
+and converts it to seconds.
+*/
+
+function convert(minutes) {
+    let convertedMinutes = minutes * 60
+    return convertedMinutes
+}
+
+console.log(convert(1))
