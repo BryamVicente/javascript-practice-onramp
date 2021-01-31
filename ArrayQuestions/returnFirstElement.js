623221ddc7b5f4f387ce64f722730001ea0c104d
github username <github email address>
2021-01-30 22:09:06 -0500

    4th question done

---
+++ ArrayQuestions/returnFirstElement.js
@@ -0,0 +1,10 @@
+/*
+Create a function that takes an array 
+containing only numbers and return the first element.
+*/
+
+function getFirstValue(arr) {
+	return arr[0]
+}
+
+console.log(getFirstValue([1,2,1,5]))
