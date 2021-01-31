e2012a20078634a4a900fe92a18439107b7505b4
github username <github email address>
2021-01-30 22:41:35 -0500

    first question done

---
+++ Intermediate Questions/calculatingBitwise.js
@@ -0,0 +1,22 @@
+/*
+Write three functions to calculate the bitwise AND, 
+bitwise OR and bitwise XOR of two numbers.
+*/
+
+function bitwiseAND(n1, n2) {
+    return n1 & n2
+}
+
+console.log(bitwiseAND(7,12))
+
+function bitwiseOR(n1, n2) {
+	return n1 | n2
+}
+
+console.log(bitwiseOR(7,12))
+
+function bitwiseXOR(n1, n2) {
+	return n1 ^ n2
+}
+
+console.log(bitwiseXOR(7,12))
